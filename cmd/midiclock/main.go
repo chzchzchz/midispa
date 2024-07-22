06ff5e67e766b48adc53620ff87da3e8fcfed6ab
chzchzchz <chzchzchz@users.noreply.github.com>
2024-07-21 23:06:06 -0700

    midiclock: continue and stop message support
    Optionally start clock in running mode (-r).
    Send midi stop messages (FC) to stop clock messages.
    Send midi continue messages (FB) to continue clock messages.

--- cmd/midiclock/main.go
+++ cmd/midiclock/main.go
@@ -47,13 +47,25 @@ const CcBpmMsb = 16
 const CcBpmLsb = 16 + 32
 
 func main() {
-	cnFlag := flag.String("name", "midiclock", "midi client name")
+	cnFlag := "midiclock"
+	flag.StringVar(&cnFlag, "name", "midiclock", "midi client name")
+	flag.StringVar(&cnFlag, "n", "midiclock", "midi client name")
+
 	bpmFlag := flag.Float64("bpm", 120.0, "send clock signals at given bpm")
 	randPctFlag := flag.Float64("randpct", 0.0, "percentage to swing the clock")
 
+	runFlag := false
+	flag.BoolVar(&runFlag, "run", false, "run at start")
+	flag.BoolVar(&runFlag, "r", false, "run at start")
+
 	flag.Parse()
+
+	if !runFlag {
+		fmt.Printf("beginning in %q stopped mode\n", cnFlag)
+	}
+
 	// Create midi sequencer for reading/writing events.
-	aseq, err := alsa.OpenSeq(*cnFlag)
+	aseq, err := alsa.OpenSeq(cnFlag)
 	if err != nil {
 		panic(err)
 	}
@@ -88,17 +100,20 @@ func main() {
 
 	curBpmInt := int(*bpmFlag * 64.0)
 
-	var clockDur int64
+	clockDur := int64(0)
 	updateClockDur := func() {
 		cps := ((float64(curBpmInt) / 64.0) / 60.0) * PPQN
 		dur := time.Duration(float64(time.Second) / cps)
 		atomic.StoreInt64(&clockDur, int64(dur))
 		fmt.Printf("bpm = %v\n", float64(curBpmInt)/64.0)
 	}
-	updateClockDur()
+	if runFlag {
+		updateClockDur()
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
+	contc := make(chan struct{}, 1)
 	go func() {
 		defer wg.Done()
 		for {
@@ -109,7 +124,15 @@ func main() {
 			cmd := ev.Data[0]
 			if cmd == midi.Clock {
 				outc <- ev
-			} else if midi.IsCC(ev.Data[0]) {
+			} else if cmd == midi.Stop {
+				atomic.StoreInt64(&clockDur, int64(0))
+			} else if cmd == midi.Continue {
+				updateClockDur()
+				select {
+				case contc <- struct{}{}:
+				default:
+				}
+			} else if midi.IsCC(cmd) {
 				cc, v := ev.Data[1], ev.Data[2]
 				if cc == CcBpmLsb {
 					curBpmInt = ((curBpmInt >> 7) << 7) | int(v)
@@ -123,27 +146,36 @@ func main() {
 	}()
 
 	randCoef := *randPctFlag / 100.0
+	clockWriter := func() {
+		defer wg.Done()
+		evClock := alsa.SeqEvent{alsa.SubsSeqAddr, []byte{midi.Clock}}
+		nextClock := time.Now()
+		for {
+			err := aseq.Write(evClock)
+			if err != nil {
+				panic(err)
+			}
+			var nextDur time.Duration
+			for {
+				nextDur = time.Duration(atomic.LoadInt64(&clockDur))
+				if nextDur != 0 {
+					break
+				}
+				<-contc
+				nextClock = time.Now()
+			}
+
+			swing := randCoef * (2.0 * (rand.Float64() - 0.5))
+			nextDurSwing := time.Duration(swing * float64(nextDur))
+			nextClockSwing := nextClock.Add(nextDurSwing)
+			nextClock = nextClock.Add(nextDur)
+			//fmt.Println(nextDur, nextDurSwing)
+			time.Sleep(time.Until(nextClockSwing))
+		}
+	}
 	if bpmFlag != nil {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			evClock := alsa.SeqEvent{alsa.SubsSeqAddr, []byte{midi.Clock}}
-			nextClock := time.Now()
-			for {
-				err := aseq.Write(evClock)
-				if err != nil {
-					panic(err)
-				}
-				nextDur := time.Duration(atomic.LoadInt64(&clockDur))
-
-				swing := randCoef * (2.0 * (rand.Float64() - 0.5))
-				nextDurSwing := time.Duration(swing * float64(nextDur))
-				nextClockSwing := nextClock.Add(nextDurSwing)
-				nextClock = nextClock.Add(nextDur)
-				//fmt.Println(nextDur, nextDurSwing)
-				time.Sleep(time.Until(nextClockSwing))
-			}
-		}()
+		go clockWriter()
 	}
 	wg.Wait()
 }
