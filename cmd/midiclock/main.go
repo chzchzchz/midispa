fc0f2321f0ce2fe4f5664723d52990e36016fd27
chzchzchz <chzchzchz@users.noreply.github.com>
2025-04-28 06:22:06 -0700

    midiclock: add -i flag for mmc commands from keyboard

--- cmd/midiclock/main.go
+++ cmd/midiclock/main.go
@@ -13,10 +13,18 @@ const PPQN = 24.0
 const CcBpmMsb = 16
 const CcBpmLsb = 16 + 32
 
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	cnFlag := "midiclock"
 	flag.StringVar(&cnFlag, "name", "midiclock", "midi client name")
 	flag.StringVar(&cnFlag, "n", "midiclock", "midi client name")
+
+	inFlag := flag.String("i", "", "input midi port")
 
 	bpmFlag := flag.Float64("bpm", 120.0, "send clock signals at given bpm")
 	randPctFlag := flag.Float64("randpct", 0.0, "percentage to swing the clock")
@@ -28,13 +36,17 @@ func main() {
 	flag.Parse()
 
 	if !runFlag {
-		log.Printf("started %q in stopped mode\n", cnFlag)
+		log.Printf("%q started in stopped mode\n", cnFlag)
 	}
 
 	// Create midi sequencer for reading/writing events.
 	aseq, err := alsa.OpenSeq(cnFlag)
-	if err != nil {
-		panic(err)
+	must(err)
+	if *inFlag != "" {
+		log.Printf("%q opened input %s", cnFlag, *inFlag)
+		sa, err := aseq.PortAddress(*inFlag)
+		must(err)
+		must(aseq.OpenPortRead(sa))
 	}
 
 	s := NewClockSequencer(aseq, bpmFlag, *randPctFlag)
--- cmd/midirec/clocked_record.sh
+++ cmd/midirec/clocked_record.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
+set -eoux pipefail
+
 mkdir -p output
 
-killall midiclock midirec midifilter midiloop
+killall midiclock midirec midifilter midiloop || true
 
-../midifilter/midifilter  -bpf ../midifilter/examples/song_select.elf  -i "W-BW61 MIDI 1" &
+# pads into song select slots
+../midifilter/midifilter -i "W-BW61 MIDI 1" -name f1 -bpf ../midifilter/examples/song_select.elf &
 pids="$!"
-../midiclock/midiclock -bpm 120 &
-pids="$! $pids"
 
-# wait for inputs to launch
+# play button into play/stop toggle button
+../midifilter/midifilter -i "W-BW61 MIDI 1" -name f2 -bpf ../midifilter/examples/mmc_play_stop.elf &
+pids="$! $pids"
 sleep 1s
 
-
-../midiloop/midiloop -i midifilter -c midiclock -dir output/slots -o "MIDI4x4 MIDI Out 1" &
-./midirec -q -i midifilter -m midiclock -o output/ &
+# clock start/stop is controlled by play button
+../midiclock/midiclock -i f2 -bpm 120 &
 pids="$! $pids"
 
+sleep 1s
+
+# use song select pads to select playing loops
+../midiloop/midiloop -i f1 -c midiclock -dir output/slots -o "MIDI4x4 MIDI Out 1" &
+
+# use song select pads to select save slot
+./midirec -q -i f1 -m midiclock -o output/ &
+pids="$! $pids"
+
+# connect clock to external midi drum machine
 clock_port=`aconnect -l | grep midiclock | cut -f1 -d: | awk ' {print $2 } ' | head -n1`
 midi_port=`aconnect -l | grep MIDI4x4 | cut -f1 -d: | awk ' { print $2 } ' | head -n1`
-
 aconnect $clock_port:0 $midi_port:2
 
 wait $pids
